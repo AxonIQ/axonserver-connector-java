3780cfa35ebbed016d06c4375ef0399eefd20dd8
Allard Buijze <allard.buijze@axoniq.io>
2020-06-13 21:58:31 +0200

    Attempt to fix some more concurrency issues in the tests

--- src/test/java/io/axoniq/axonserver/connector/impl/InstructionChannelTest.java
+++ src/test/java/io/axoniq/axonserver/connector/impl/InstructionChannelTest.java
@@ -10,8 +10,13 @@ import io.axoniq.axonserver.connector.instruction.ProcessorInstructionHandler;
 import io.axoniq.axonserver.grpc.control.EventProcessorInfo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.shaded.okhttp3.Request;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
@@ -28,6 +33,7 @@ import static org.mockito.Mockito.verify;
 class InstructionChannelTest extends AbstractAxonServerIntegrationTest {
 
     private AxonServerConnectionFactory client;
+    private static final Logger logger = LoggerFactory.getLogger(InstructionChannelTest.class);
 
     @AfterEach
     void tearDown() {
@@ -45,6 +51,7 @@ class InstructionChannelTest extends AbstractAxonServerIntegrationTest {
         connection1.instructionChannel().enableHeartbeat(500, 500, TimeUnit.MILLISECONDS);
 
         assertWithin(2, TimeUnit.SECONDS, () -> assertTrue(connection1.isReady()));
+        logger.info("Connection status is READY");
 
         // we make sure the heartbeat doesn't complain at first
         long endCheck = System.currentTimeMillis() + 800;
@@ -53,13 +60,17 @@ class InstructionChannelTest extends AbstractAxonServerIntegrationTest {
             Thread.sleep(100);
         }
 
+        logger.info("Simulating bad connection");
         Timeout connectionIssue = axonServerProxy.toxics().timeout("bad_connection", ToxicDirection.DOWNSTREAM, Long.MAX_VALUE);
 
-        assertWithin(4, TimeUnit.SECONDS, () -> assertFalse(connection1.isConnected()));
+        logger.info("Waiting for connector to acknowledge broken connection");
+        assertWithin(5, TimeUnit.SECONDS, () -> assertFalse(connection1.isConnected()));
 
+        logger.info("Ending bad connection simulation");
         connectionIssue.remove();
 
-        assertWithin(4, TimeUnit.SECONDS, () -> assertTrue(connection1.isReady()));
+        logger.info("Waiting for connector to establish connection");
+        assertWithin(5, TimeUnit.SECONDS, () -> assertTrue(connection1.isReady()));
     }
 
     @Test
@@ -91,10 +102,10 @@ class InstructionChannelTest extends AbstractAxonServerIntegrationTest {
         connection1.instructionChannel().registerEventProcessor("testProcessor", processorInfo::get,
                                                                 instructionHandler);
 
-        sendToAxonServer(Request.Builder::patch, "/v1/components/" + getClass().getSimpleName() + "/processors/testProcessor/pause?context=default");
-
-        assertWithin(1, TimeUnit.SECONDS, () -> verify(instructionHandler).pauseProcessor());
-
+        assertWithin(1, TimeUnit.SECONDS, () -> {
+            sendToAxonServer(Request.Builder::patch, "/v1/components/" + getClass().getSimpleName() + "/processors/testProcessor/pause?context=default");
+            verify(instructionHandler).pauseProcessor();
+        });
         processorInfo.set(buildEventProcessorInfo(false));
 
 
@@ -131,16 +142,16 @@ class InstructionChannelTest extends AbstractAxonServerIntegrationTest {
                                             .routingServers(axonServerAddress)
                                             .build();
         AxonServerConnection connection1 = client.connect("default");
-        ProcessorInstructionHandler instructionHandler = mock(ProcessorInstructionHandler.class);
+        StubProcessorInstructionHandler instructionHandler = new StubProcessorInstructionHandler();
         AtomicReference<EventProcessorInfo> processorInfo = new AtomicReference<>(buildEventProcessorInfo(true));
 
         connection1.instructionChannel().registerEventProcessor("testProcessor", processorInfo::get,
                                                                 instructionHandler);
 
 
-        assertWithin(1, TimeUnit.SECONDS, () -> {
+        assertWithin(2, TimeUnit.SECONDS, () -> {
             sendToAxonServer(Request.Builder::patch, "/v1/components/" + getClass().getSimpleName() + "/processors/testProcessor/segments/0/move?context=default&target=foo");
-            verify(instructionHandler).releaseSegment(eq(0));
+            assertTrue(instructionHandler.instructions.contains("release0"));
         });
     }
 
@@ -170,5 +181,40 @@ class InstructionChannelTest extends AbstractAxonServerIntegrationTest {
                                                                          .build())
                                  .build();
 
+    }
+
+    private static class StubProcessorInstructionHandler implements ProcessorInstructionHandler {
+
+        private final List<String> instructions = new CopyOnWriteArrayList<>();
+
+        @Override
+        public CompletableFuture<Boolean> releaseSegment(int segmentId) {
+            instructions.add("release" + segmentId);
+            return CompletableFuture.completedFuture(true);
+        }
+
+        @Override
+        public CompletableFuture<Boolean> splitSegment(int segmentId) {
+            instructions.add("split" + segmentId);
+            return CompletableFuture.completedFuture(true);
+        }
+
+        @Override
+        public CompletableFuture<Boolean> mergeSegment(int segmentId) {
+            instructions.add("merge" + segmentId);
+            return CompletableFuture.completedFuture(true);
+        }
+
+        @Override
+        public CompletableFuture<Void> pauseProcessor() {
+            instructions.add("pause");
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public CompletableFuture<Void> startProcessor() {
+            instructions.add("start");
+            return CompletableFuture.completedFuture(null);
+        }
     }
 }
