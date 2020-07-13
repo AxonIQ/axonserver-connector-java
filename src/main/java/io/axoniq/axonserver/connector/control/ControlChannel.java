d0b4968b4eecd35576a717dd9c64f695ad323f93
Allard Buijze <allard.buijze@axoniq.io>
2020-07-13 14:29:48 +0200

    Added copyright

--- src/main/java/io/axoniq/axonserver/connector/control/ControlChannel.java
+++ src/main/java/io/axoniq/axonserver/connector/control/ControlChannel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020. AxonIQ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.axoniq.axonserver.connector.control;
 
 import io.axoniq.axonserver.connector.InstructionHandler;
@@ -6,7 +22,6 @@ import io.axoniq.axonserver.grpc.InstructionAck;
 import io.axoniq.axonserver.grpc.control.EventProcessorInfo;
 import io.axoniq.axonserver.grpc.control.PlatformInboundInstruction;
 import io.axoniq.axonserver.grpc.control.PlatformOutboundInstruction;
-import io.axoniq.axonserver.grpc.event.Confirmation;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
