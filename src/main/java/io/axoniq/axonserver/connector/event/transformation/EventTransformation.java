1684109f6bd334740def4c938754edaf49c9d3fa
Sara Pellegrini <sara.pellegrini@gmail.com>
2023-06-15 09:43:07 +0200

    Java Doc for EventTransformation

--- src/main/java/io/axoniq/axonserver/connector/event/transformation/EventTransformation.java
+++ src/main/java/io/axoniq/axonserver/connector/event/transformation/EventTransformation.java
@@ -17,26 +17,66 @@
 package io.axoniq.axonserver.connector.event.transformation;
 
 /**
+ * The Event Transformation details.
+ *
  * @author Sara Pellegrini
  * @since 2023.0.0
  */
 public interface EventTransformation {
 
+    /**
+     * Returns the identifier of the EventTransformation.
+     * @return the identifier of the EventTransformation.
+     */
     String id();
 
+    /**
+     * Returns the description of the EventTransformation.
+     * @return the description of the EventTransformation.
+     */
     String description();
 
+    /**
+     * Each transformation action registered in the transformation has its own incremental sequence.
+     * An action could represent the deletion of an Event, or its replacement.
+     * This method returns the sequence of the last transformation action registered in the EventTransformation.
+     * @return the last sequence of the EventTransformation.
+     */
     Long lastSequence();
 
+    /**
+     * Returns the state of the EventTransformation.
+     * @return the state of the EventTransformation.
+     */
     State state();
 
+    /**
+     * The state of the transformation
+     */
     enum State {
 
+        /**
+         * A transformation is in ACTIVE state when it is still open to accept new transformation actions.
+         * A new transformation is created in ACTIVE state.
+         */
         ACTIVE,
+
+        /**
+         * A transformation is in APPLYING state when the process to apply the transformation actions against the event
+         * store is in progress. In this state, the transformation cannot accept any further transformation action.
+         */
         APPLYING,
 
+        /**
+         * A transformation is in APPLIED state when all the transformation actions registered in it have been applied
+         * to the event store.
+         */
         APPLIED,
 
+        /**
+         * A transformation is in CANCELLED state when it has been discarded before to be applied. In this state, the
+         * transformation cannot accept any further transformation action, and cannot be applied.
+         */
         CANCELLED
     }
 }
