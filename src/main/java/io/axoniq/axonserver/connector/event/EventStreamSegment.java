a38a8e85479825d97f45c72334d3bf3b8cf676a3
Marc <marc.gathier@axoniq.io>
2024-02-15 10:59:10 +0100

    more javadoc

--- src/main/java/io/axoniq/axonserver/connector/event/EventStreamSegment.java
+++ src/main/java/io/axoniq/axonserver/connector/event/EventStreamSegment.java
@@ -3,9 +3,29 @@ package io.axoniq.axonserver.connector.event;
 import io.axoniq.axonserver.connector.ResultStream;
 import io.axoniq.axonserver.grpc.event.EventWithToken;
 
+/**
+ * An event stream producing events for one segment of a persistent stream.
+ */
 public interface EventStreamSegment extends ResultStream<EventWithToken> {
+
+    /**
+     * Registers a callback that will be invoked when Axon Server closes the segment within the persistent
+     * stream connection. This happens when the number of segments in the persistent stream has changed or when
+     * Axon Server assigns a segment to another client.
+     * @param callback the callback to register
+     */
     void onSegmentClosed(Runnable callback);
+
+    /**
+     * Sends the last processed token for this segment to Axon Server. Clients may choose to notify each processed event,
+     * or to only sent progress information after a number of processed events.
+     * @param token the last processed token for this segment
+     */
     void progress( long token);
 
+    /**
+     * Returns the segment number of the stream.
+     * @return the segment number of this stream
+     */
     int segment();
 }
--- src/main/java/io/axoniq/axonserver/connector/event/PersistentStream.java
+++ src/main/java/io/axoniq/axonserver/connector/event/PersistentStream.java
@@ -2,11 +2,25 @@ package io.axoniq.axonserver.connector.event;
 
 import java.util.function.Consumer;
 
+/**
+ * A connection to a persistent stream. Axon Server can assign zero or more segments to this connection.
+ */
 public interface PersistentStream {
 
+    /**
+     * Registers a callback to invoke when Axon Server assigns a segment to this connection.
+     * @param callback the callback to invoke when a segment is opened
+     */
     void onSegmentOpened(Consumer<EventStreamSegment> callback);
 
+    /**
+     * Closes the persistent stream.
+     */
     void close();
 
+    /**
+     * Registers a callback to invoke when Axon Server closes this connection (or the connection to Axon Server is lost).
+     * @param closedCallback the callback to invoke when the persistent stream is closed
+     */
     void onClosed(Consumer<Throwable> closedCallback);
 }
