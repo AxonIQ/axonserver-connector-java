0f412e72b4d37299517b40d47450d88b4417efd8
Allard Buijze <allard.buijze@axoniq.io>
2025-11-24 09:02:54 +0100

    Connector must invoke callback on registration error
    When registering a command or event handler, the returned registration should invoke any registered acknowledgement callbacks, whether that registration was successful or not. An additional callback method is added to allow distinguishing between these situations.
    
    This issue is the root cause of AxonFramework/AxonFramework#3938

--- src/main/java/io/axoniq/axonserver/connector/Registration.java
+++ src/main/java/io/axoniq/axonserver/connector/Registration.java
@@ -16,19 +16,21 @@
 
 package io.axoniq.axonserver.connector;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
 
 /**
- * Interface describing an instruction to perform a registration, which can be cancelled.
+ * Interface describing an instruction to perform a registration, which can be canceled.
  */
 @FunctionalInterface
 public interface Registration {
 
     /**
      * Cancel the registration from which this instance was returned. Does nothing if the registration has already been
-     * cancelled, or when the registration was undone by another mechanism (such as a new registration overriding this
+     * canceled, or when the registration was undone by another mechanism (such as a new registration overriding this
      * one).
      *
      * @return a {@link CompletableFuture} of {@link Void} to react when {@code this} {@link Registration} has been
@@ -44,7 +46,7 @@ public interface Registration {
      * @param timeout the duration to wait until the operation has been acknowledged
      * @param unit    the {@link TimeUnit} for the given {@code timeout} to wait until the operation has been
      *                acknowledged
-     * @return {@code this} {@link Registration} to support a fluent API
+     * @return this Registration to support a fluent API
      * @throws TimeoutException     is thrown when the given {@code timeout} and {@code unit} is surpassed
      * @throws InterruptedException is thrown when the thread waiting for the acknowledgement is interrupted
      */
@@ -56,13 +58,31 @@ public interface Registration {
      * Registers the given {@code runnable} to {@code this} {@link Registration} to be executed when the acknowledgement
      * of {@code this} {@link Registration} is received. Allows for the addition of further logic to {@code this
      * Registration}, like invoking {@link #awaitAck(long, TimeUnit)} for example.
+     * <p/>
+     * The given {@code runnable} is invoked, regardless of successful or failed acknowledgement. Use
+     * {@link #onAck(Consumer)} to register an action that needs to distinguish between successful and failed
+     * registration.
      *
      * @param runnable the {@link Runnable} to execute when the acknowledgement of {@code this} {@link Registration} is
      *                 received
-     * @return {@code this} {@link Registration} to support a fluent API
+     * @return this Registration to support a fluent API
      */
     default Registration onAck(Runnable runnable) {
         runnable.run();
+        return this;
+    }
+
+    /**
+     * Registers the given {@code runnable} to {@code this} {@link Registration} to be executed when the acknowledgement
+     * of {@code this} {@link Registration} is received. Allows for the addition of further logic to
+     * {@code this Registration}, like invoking {@link #awaitAck(long, TimeUnit)} for example.
+     *
+     * @param action the action to execute when the acknowledgement of {@code this} {@link Registration} is received,
+     *               either normally or exceptionally.
+     * @return this Registration to support a fluent API
+     */
+    default Registration onAck(Consumer<Optional<Throwable>> action) {
+        action.accept(Optional.empty());
         return this;
     }
 }
